3277d06ddc3deaad0588591210af306a40cf415c
iswoad <iswoadansary@gmail.com>
2021-06-25 21:38:32 +0600

    added animation to services

--- src/components/Home/ServiceDetails/ServiceDetails.js
+++ src/components/Home/ServiceDetails/ServiceDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import './ServiceDetails.css'
 
 const ServiceDetails = (props) => {
     const { image, title, description, _id } = props.service;
@@ -12,7 +13,7 @@ const ServiceDetails = (props) => {
 
 
     return (
-        <div className = "col-md-4 text-center">
+        <div className = "col-md-4 text-center sDetails">
             <img style={{height: '200px'}} src={`data:image/png;base64,${image.img}`} alt = "services"/>
             <h5 className = "mt-3 mb-3">{title}</h5>
             <hr style = {{border: '5px solid yellow', width: '30%', margin: '5px auto'}} />
